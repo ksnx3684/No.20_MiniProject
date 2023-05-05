90b7aa0123d42822f871825aad95bf894e2e20de
JeongHawook <saroball3@gmail.com>
2023-05-06 04:43:21 +0900

    add:comments 3 layered

---
+++ repositories/comments.repository.js
@@ -0,0 +1,49 @@
+class CommentsRepository {
+    constructor(commentsModel) {
+        this.commentsModel = commentsModel;
+    }
+
+    allComments = async (_nickname, _postId) => {
+        const allComments = await this.commentsModel.findAll({
+            where: { postId: _postId, nickname: _nickname },
+        });
+    };
+
+    oneComment = async (_commentId) => {
+        const oneComment = await this.commentsModel.findByPk(_commentId);
+    };
+
+    //가져온 닉네임 어케 처리할까1
+    createComment = async (comment, _nickname, _postId, nickname, userId) => {
+        const createComment = await this.commentsModel.create({
+            comment,
+            nickname: nickname,
+            postId: _postId,
+            userId: userId,
+        });
+    };
+
+    updateComment = async (
+        comment,
+        //    _nickname,
+        _postId,
+        nickname,
+        userId,
+        _commentId
+    ) => {
+        const updateComment = await this.commentsModel.update(
+            { comment },
+            {
+                where: { _commentId },
+            }
+        );
+    };
+    deleteComment = async (_nickname, _postId) => {
+        const deleteComment = await this.commentsModel.destroy({
+            where: {
+                commentId: _commentId,
+            },
+        });
+    };
+}
+module.exports = CommentsRepository;
