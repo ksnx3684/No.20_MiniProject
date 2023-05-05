880809b5f2e7be43a676651be1ff538b856c7622
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-05-05 21:21:42 +0900

    fix : app.js에 errorHander 미들웨어 불러오기 추가

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-const cookieParser = require('cookie-parser');
-const router = require('./routes');
+const cookieParser = require("cookie-parser");
+const router = require("./routes");
+
+const errorHandler = require("./ middlewares/errorHandler");
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api', router);
+app.use("/api", router);
+
+app.use(errorHandler);
 
 app.listen(port, () => {
-  console.log('Server open on port', port);
+  console.log("Server open on port", port);
 });
