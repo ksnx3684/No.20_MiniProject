fba9d3443dd2ce5d473e0a77ffb2f09b9bd0d4c7
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-05-05 15:52:21 +0900

    Chore: 모델, 마이그레이션 생성

---
+++ Back-End/models/posts.js
@@ -0,0 +1,72 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Posts extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.belongsTo(models.Users, {
+        targetKey: "UserId",
+        foreignKey: "userId",
+        onDelete: "CASCADE",
+      });
+
+      this.hasMany(models.Comments, {
+        sourceKey: "postId",
+        foreignKey: "PostId",
+        onDelete: "CASCADE",
+      });
+    }
+  }
+  Posts.init(
+    {
+      postId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      UserId: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      content: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+      likes: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+      status: {
+        allowNull: false,
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Posts",
+    }
+  );
+  return Posts;
+};
