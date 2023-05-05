821d9e65f8ee38e01003559e7074ac5444d173c0
ksnx3684 <ksnx3684@naver.com>
2023-05-05 13:40:19 +0900

    Chore : 초기 Setting

---
+++ Back-End/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const cookieParser = require('cookie-parser');
+const router = require('./routes');
+
+app.use(express.json());
+app.use(cookieParser());
+app.use('/api', router);
+
+app.listen(port, () => {
+  console.log('Server open on port', port);
+});
