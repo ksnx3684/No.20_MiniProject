b08678595940818f56acad517d9030eb5130dd06
kjlee <59638614+dlruawo07@users.noreply.github.com>
2023-05-06 11:22:31 +0900

    fix : 수정

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -9,8 +9,9 @@ class PostsController {
     try {
       const posts = await this.postsService.findAllPosts();
 
-      res.status(200).json({ posts });
+      return res.status(200).json({ posts, result: true });
     } catch (e) {
+      e.failedApi = "게시글 조회";
       next(e);
     }
   };
@@ -21,8 +22,9 @@ class PostsController {
 
       const posts = await this.postsService.findUserPosts(nickname);
 
-      res.status(200).json({ posts });
+      res.status(200).json({ posts, result: true });
     } catch (e) {
+      e.failedApi = "게시글 조회";
       next(e);
     }
   };
@@ -45,13 +47,12 @@ class PostsController {
 
       await this.postsService.createPost(userId, nickname, title, content);
 
-      res.status(201).json({ message: "게시글을 작성했습니다." });
+      return res.status(201).send(true);
     } catch (e) {
       e.failedApi = "게시글 작성";
       next(e);
     }
   };
-
 
   // 게시글 상세 조회
   getOnePost = async (req, res, next) => {
@@ -60,18 +61,22 @@ class PostsController {
       const post = await this.postService.getOnePost(_nickname, _postId);
 
       if (!post)
-          return res.status(404).json({ errorMessage: '게시글이 존재하지 않습니다.' });
+        return res
+          .status(404)
+          .json({ errorMessage: "게시글이 존재하지 않습니다." });
 
       const checkPost = await this.postService.checkPost(_postId);
       const prePost = await this.postService.getPrePost(_nickname, _postId);
       const nextPost = await this.postService.getNextPost(_nickname, _postId);
-        
+
       return res.status(200).json({ post: post });
     } catch (err) {
       console.log(err);
-      return res.status(400).json({ errorMessage: '게시글 조회에 실패하였습니다.' });
+      return res
+        .status(400)
+        .json({ errorMessage: "게시글 조회에 실패하였습니다." });
     }
-  }
+  };
 
   // 게시글 수정
   updatePost = async (req, res, next) => {
@@ -79,52 +84,68 @@ class PostsController {
       const { _nickname, _postId } = req.params;
       const { title, content } = req.body;
       const { nickname } = res.locals.user;
-            
+
       const post = await this.postService.checkPost(_postId);
 
-      if(!post)
-        return res.status(404).json({ errorMessage: '게시글이 존재하지 않습니다.' });
-            
-      if(post.nickname !== nickname)
-        return res.status(403).json({ errorMessage: '게시글 수정 권한이 존재하지 않습니다.' });
+      if (!post)
+        return res
+          .status(404)
+          .json({ errorMessage: "게시글이 존재하지 않습니다." });
 
-      await this.postService.updatePost(_postId, title, content)
+      if (post.nickname !== nickname)
+        return res
+          .status(403)
+          .json({ errorMessage: "게시글 수정 권한이 존재하지 않습니다." });
+
+      await this.postService
+        .updatePost(_postId, title, content)
         .catch((err) => {
           console.log(err);
-          return res.status(400).json({ errorMessage: '게시글 수정이 정상적으로 처리되지 않았습니다.' });
+          return res.status(400).json({
+            errorMessage: "게시글 수정이 정상적으로 처리되지 않았습니다.",
+          });
         });
     } catch (err) {
       console.log(err);
-      return res.status(400).json({ errorMessage: '게시글 수정에 실패하였습니다.' });
+      return res
+        .status(400)
+        .json({ errorMessage: "게시글 수정에 실패하였습니다." });
     }
-      return res.status(200).json({ message: '게시글 수정에 성공하였습니다.' });
-  }
+    return res.status(200).json({ message: "게시글 수정에 성공하였습니다." });
+  };
 
   // 게시글 삭제
   deletePost = async (req, res, next) => {
     try {
       const { _nickname, _postId } = req.params;
       const { nickname } = res.locals.user;
-      
+
       const post = await this.postService.checkPost(_postId);
-      
+
       if (!post)
-        return res.status(404).json({ errorMessage: '게시글이 존재하지 않습니다.' });
+        return res
+          .status(404)
+          .json({ errorMessage: "게시글이 존재하지 않습니다." });
 
       if (!nickname || post.nickname !== nickname)
-        return res.status(403).json({ errorMessage: '게시글 삭제 권한이 존재하지 않습니다.' });
+        return res
+          .status(403)
+          .json({ errorMessage: "게시글 삭제 권한이 존재하지 않습니다." });
 
-      await this.postService.deletePost(nickname, _postId)
-        .catch((err) => {
-          console.log(err);
-          return res.status(400).json({ errorMessage: '게시글이 정상적으로 삭제되지 않았습니다.' });
-        });
+      await this.postService.deletePost(nickname, _postId).catch((err) => {
+        console.log(err);
+        return res
+          .status(400)
+          .json({ errorMessage: "게시글이 정상적으로 삭제되지 않았습니다." });
+      });
     } catch (err) {
       console.log(err);
-      return res.status(400).json({ errorMessage: '게시글 삭제에 실패하였습니다.' });
+      return res
+        .status(400)
+        .json({ errorMessage: "게시글 삭제에 실패하였습니다." });
     }
-      return res.status(200).json({ message: '게시글 삭제에 성공하였습니다.' });
-  }
+    return res.status(200).json({ message: "게시글 삭제에 성공하였습니다." });
+  };
 }
 
 module.exports = PostsController;
