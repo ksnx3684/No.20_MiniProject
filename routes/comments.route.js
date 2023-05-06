3ce3590291f157fe58b5bf7542284068fb571dd2
ksnx3684 <ksnx3684@naver.com>
2023-05-06 17:29:21 +0900

    fix : route 오류 수정

--- routes/comments.route.js
+++ routes/comments.route.js
@@ -4,7 +4,6 @@ const authMiddleware = require("../middlewares/auth-middleware");
 const CommentsController = require("../controllers/comments.controller");
 const commentsController = new CommentsController();
 
-router.get("/", commentsController.allComments);
 router.post("/", authMiddleware, commentsController.createComment);
 router.put("/:_commentId", authMiddleware, commentsController.updateComment);
 router.delete("/:_commentId", authMiddleware, commentsController.deleteComment);
