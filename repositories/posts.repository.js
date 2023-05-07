c6ed8b2e61bc6bfbfa8411b1be509f9119394419
JeongHawook <saroball3@gmail.com>
2023-05-07 12:24:22 +0900

    edit:상세페이지

--- repositories/posts.repository.js
+++ repositories/posts.repository.js
@@ -1,86 +1,103 @@
 const { Op } = require("sequelize");
 
 class PostsRepository {
-  constructor(model) {
-    this.model = model;
-  }
-    
-  findAllPosts = async () => {
-    return await this.model.findAll({ where: { status: true } });
-  };
+    constructor(model, comModel) {
+        this.model = model;
+        this.comModel = comModel;
+    }
 
-  findUserPosts = async (nickname) => {
-    return await this.model.findAll({ where: { nickname, status: true } });
-  };
+    findAllPosts = async () => {
+        return await this.model.findAll({ where: { status: true } });
+    };
 
-  createPost = async (userId, nickname, title, content) => {
-    await this.model.create({ UserId: userId, nickname, title, content });
-  };
+    findUserPosts = async (nickname) => {
+        return await this.model.findAll({ where: { nickname, status: true } });
+    };
 
-  getOnePost = async (_postId) => {
-    const post = await this.model.findOne({
-      where: { postId: _postId, status: true },
-      attributes: [
-        'postId',
-        'UserId',
-        'title',
-        'content',
-        'likes',
-        'status',
-        'createdAt',
-        'updatedAt',
-      ],
-    });
-      return post;
-  };
+    createPost = async (userId, nickname, title, content) => {
+        await this.model.create({ UserId: userId, nickname, title, content });
+    };
 
-  getPrevPost = async (_postId) => {
-    const post = await this.model.findOne({
-      where: {
-        postId: {
-          [Op.lt]: _postId
-        },
-          status: true
-      },
-      order: [['postId', 'DESC']],
-    });
-      return post;
-  };
+    getOnePost = async (_postId) => {
+        const post = await this.model.findOne({
+            where: { postId: _postId, status: true },
+            attributes: [
+                "postId",
+                "UserId",
+                "title",
+                "content",
+                "likes",
+                "status",
+                "createdAt",
+                "updatedAt",
+            ],
+            include: [
+                {
+                    model: this.comModel,
+                    attributes: [
+                        "commentId",
+                        "comment",
+                        "nickname",
+                        "createdAt",
+                        "updatedAt",
+                    ],
+                },
+            ],
+            where: {
+                postId: _postId,
+            },
+        });
+        console.log(post);
+        return post;
+    };
 
-  getNextPost = async (_postId) => {
-    const post = await this.model.findOne({
-      where: {
-        postId: {
-          [Op.gt]: _postId
-        },
-          status: true
-        }
-    });
-      return post;
-  };
+    getPrevPost = async (_postId) => {
+        const post = await this.model.findOne({
+            where: {
+                postId: {
+                    [Op.lt]: _postId,
+                },
+                status: true,
+            },
+            order: [["postId", "DESC"]],
+        });
+        return post;
+    };
 
-  checkPost = async (_postId) => {
-    const post = await this.model.findOne({
-      where: { postId: _postId, status: true },
-    });
-    return post;
-  };
+    getNextPost = async (_postId) => {
+        const post = await this.model.findOne({
+            where: {
+                postId: {
+                    [Op.gt]: _postId,
+                },
+                status: true,
+            },
+        });
+        return post;
+    };
 
-  updatePost = async (_postId, title, content) => {
-    const post = await this.model.update(
-      { title, content },
-      { where: { postId: _postId } }
-    );
-    return post;
-  };
+    checkPost = async (_postId) => {
+        const post = await this.model.findOne({
+            where: { postId: _postId, status: true },
+        });
+        return post;
+    };
 
-  deletePost = async (nickname, _postId) => {
-    const post = await this.model.update(
-      { status: false },
-      { where: { nickname, postId: _postId }}
-    );
-    return post;
-  };
+    updatePost = async (_postId, title, content) => {
+        const post = await this.model.update(
+            { title, content },
+            { where: { postId: _postId } }
+        );
+        return post;
+    };
+
+    deletePost = async (nickname, _postId) => {
+        const post = await this.model.update(
+            { status: false },
+            { where: { nickname, postId: _postId } }
+        );
+        return post;
+    };
 }
 
 module.exports = PostsRepository;
--- services/posts.service.js
+++ services/posts.service.js
@@ -2,124 +2,130 @@ const errorWithCode = require("../utils/error");
 
 const PostsRepository = require("./../repositories/posts.repository");
 const UsersRepository = require("./../repositories/users.repository");
-const { Posts, Users } = require("./../models/");
+const { Posts, Users, Comments } = require("./../models/");
 
 class PostsService {
-  postsRepository = new PostsRepository(Posts);
-  usersRepository = new UsersRepository(Users);
+    postsRepository = new PostsRepository(Posts, Comments);
+    usersRepository = new UsersRepository(Users);
 
-  findAllPosts = async () => {
-    const posts = await this.postsRepository.findAllPosts();
-    if (!posts.length) {
-      throw errorWithCode(404, "게시글이 존재하지 않습니다");
-    }
+    findAllPosts = async () => {
+        const posts = await this.postsRepository.findAllPosts();
+        if (!posts.length) {
+            throw errorWithCode(404, "게시글이 존재하지 않습니다");
+        }
 
-    // 최근 생성 순으로 정렬
-    posts.sort((a, b) => b.createdAt - a.createdAt);
+        // 최근 생성 순으로 정렬
+        posts.sort((a, b) => b.createdAt - a.createdAt);
 
-    // 게시글 갯수를 20개로 제한
-    if (posts.length > 20) {
-      posts.length = 20;
-    }
+        // 게시글 갯수를 20개로 제한
+        if (posts.length > 20) {
+            posts.length = 20;
+        }
 
-    return posts.map((post) => {
-      return {
-        postId: post.postId,
-        userId: post.UserId,
-        nickname: post.nickname,
-        title: post.title,
-        createdAt: post.createdAt,
-      };
-    });
-  };
+        return posts.map((post) => {
+            return {
+                postId: post.postId,
+                userId: post.UserId,
+                nickname: post.nickname,
+                title: post.title,
+                createdAt: post.createdAt,
+            };
+        });
+    };
 
-  findUserPosts = async (_nickname) => {
-    const user = await this.usersRepository.getUserWithNickname(_nickname);
-    if (!user) {
-      throw errorWithCode(404, "존재하지 않는 사용자입니다.");
-    }
+    findUserPosts = async (_nickname) => {
+        const user = await this.usersRepository.getUserWithNickname(_nickname);
+        if (!user) {
+            throw errorWithCode(404, "존재하지 않는 사용자입니다.");
+        }
 
-    const posts = await this.postsRepository.findUserPosts(_nickname);
-    if (!posts.length) {
-      throw errorWithCode(404, "게시글이 존재하지 않습니다");
-    }
-    // 게시글 갯수를 20개로 제한
-    if (posts.length > 20) {
-      posts.length = 20;
-    }
+        const posts = await this.postsRepository.findUserPosts(_nickname);
+        if (!posts.length) {
+            throw errorWithCode(404, "게시글이 존재하지 않습니다");
+        }
+        // 게시글 갯수를 20개로 제한
+        if (posts.length > 20) {
+            posts.length = 20;
+        }
 
-    // 최근 생성 순으로 정렬
-    posts.sort((a, b) => b.createdAt - a.createdAt);
+        // 최근 생성 순으로 정렬
+        posts.sort((a, b) => b.createdAt - a.createdAt);
 
-    return posts.map((post) => {
-      return {
-        postId: post.postId,
-        userId: post.UserId,
-        nickname: post.nickname,
-        title: post.title,
-        createdAt: post.createdAt,
-      };
-    });
-  };
+        return posts.map((post) => {
+            return {
+                postId: post.postId,
+                userId: post.UserId,
+                nickname: post.nickname,
+                title: post.title,
+                createdAt: post.createdAt,
+            };
+        });
+    };
 
-  createPost = async (userId, nickname, title, content) => {
-    await this.postsRepository.createPost(userId, nickname, title, content);
-  };
+    createPost = async (userId, nickname, title, content) => {
+        await this.postsRepository.createPost(userId, nickname, title, content);
+    };
 
-  getOnePost = async (_postId) => {
-    const post = await this.postsRepository.getOnePost(_postId);
-    if (!post)
-      throw errorWithCode(404, "게시글이 존재하지 않습니다.");
+    getOnePost = async (_postId) => {
+        const post = await this.postsRepository.getOnePost(_postId);
+        if (!post) throw errorWithCode(404, "게시글이 존재하지 않습니다.");
 
-    const prevPost = await this.postsRepository.getPrevPost(_postId);
-    const nextPost = await this.postsRepository.getNextPost(_postId);
-    if (!prevPost)
-      return {
-        nickname: post.nickname,
-        title: post.title,
-        content: post.content,
-        prevPostId: '',
-        prevPostTitle: '',
-        nextPostId: nextPost.postId,
-        nextPostTitle: nextPost.title
-      }
-    if (!nextPost)
-      return {
-        nickname: post.nickname,
-        title: post.title,
-        content: post.content,
-        prevPostId: prevPost.postId,
-        prevPostTitle: prevPost.title,
-        nextPostId: '',
-        nextPostTitle: ''
-      }
-    else {
-      return {
-        nickname: post.nickname,
-        title: post.title,
-        content: post.content,
-        prevPostId: prevPost.postId,
-        prevPostTitle: prevPost.title,
-        nextPostId: nextPost.postId,
-        nextPostTitle: nextPost.title
-      }
-    }
-  };
+        const prevPost = await this.postsRepository.getPrevPost(_postId);
+        const nextPost = await this.postsRepository.getNextPost(_postId);
+        if (!prevPost)
+            return {
+                nickname: post.nickname,
+                title: post.title,
+                content: post.content,
+                prevPostId: "",
+                prevPostTitle: "",
+                nextPostId: nextPost.postId,
+                nextPostTitle: nextPost.title,
+                postComment: post.Comments,
+            };
+        if (!nextPost)
+            return {
+                nickname: post.nickname,
+                title: post.title,
+                content: post.content,
+                prevPostId: prevPost.postId,
+                prevPostTitle: prevPost.title,
+                nextPostId: "",
+                nextPostTitle: "",
+                postComment: post.Comments,
+            };
+        else {
+            return {
+                nickname: post.nickname,
+                title: post.title,
+                content: post.content,
+                prevPostId: prevPost.postId,
+                prevPostTitle: prevPost.title,
+                nextPostId: nextPost.postId,
+                nextPostTitle: nextPost.title,
+                postComment: post.Comments,
+            };
+        }
+    };
 
-  checkPost = async (_postId) => {
-    const post = await this.postsRepository.checkPost(_postId);
-    return post;
-  };
+    checkPost = async (_postId) => {
+        const post = await this.postsRepository.checkPost(_postId);
+        return post;
+    };
 
-  updatePost = async (_postId, title, content) => {
-    const post = await this.postsRepository.updatePost(_postId, title, content);
-    return post;
-  };
+    updatePost = async (_postId, title, content) => {
+        const post = await this.postsRepository.updatePost(
+            _postId,
+            title,
+            content
+        );
+        return post;
+    };
 
-  deletePost = async (nickname, _postId) => {
-    const post = await this.postsRepository.deletePost(nickname, _postId);
-    return post;
-  };
+    deletePost = async (nickname, _postId) => {
+        const post = await this.postsRepository.deletePost(nickname, _postId);
+        return post;
+    };
 }
 
 module.exports = PostsService;
