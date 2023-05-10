d89c73f477bfb9d378f4b2f8eb6c63d22469f917
ksnx3684 <ksnx3684@naver.com>
2023-05-10 19:10:18 +0900

    feat : 게시글 상세 조회 데이터 추가

--- services/posts.service.js
+++ services/posts.service.js
@@ -89,6 +89,7 @@ class PostsService {
       nickname: post.nickname,
       title: post.title,
       content: post.content,
+      createdAt: post.createdAt,
       prevPostId: "",
       prevPostTitle: "",
       nextPostId: "",
